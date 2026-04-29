a7d4973ea6feff0b186d7436763290b0625102ea
Kevin Menor <ivanmenorsilva@gmail.com>
2026-04-29 10:49:11 -0500

    Agregar mas información en page/Nosotros

--- src/pages/Nosotros.tsx
+++ src/pages/Nosotros.tsx
@@ -1,10 +1,14 @@
 import '../App.css'
 import Footer from '../components/Footer'
 import ProcesoCafe from "../components/ProcesoCafe";
+import HeroNosotros from '../components/HeroNosotros';
+import IntroProceso from '../components/IntroProceso';
 
 function Nosotros() {
     return (
         <>
+            <HeroNosotros />
+            <IntroProceso />
             <ProcesoCafe />
             <Footer />
         </>
